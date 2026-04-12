3934666f121dbe4c7e0e0bad548d9893709725af
Godwin Atsali <godwinatsali880@gmail.com>
2026-04-12 20:48:54 +0300

    Delete postgres.session.sql

--- postgres.session.sql
+++
@@ -1,7 +0,0 @@
-SELECT 
-    products.product_name,
-    products.product_id,
-    categories.category_name
-FROM order_items
-LEFT JOIN products ON order_items.product_id = products.product_id
-LEFT JOIN categories ON products.category_id = categories.category_id
